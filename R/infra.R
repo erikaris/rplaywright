068b3e01c3ac9cc7ea351128d18bc19690ccf5f5
Erika Siregar <erika.mukhlisina@gmail.com>
2024-04-21 18:04:17 +0100

    reademe and infra.r

---
+++ R/infra.R
@@ -0,0 +1,198 @@
+#### install npm ####
+#' @export
+rplaywright_npm_install <- function(
+    force = FALSE
+){
+  # Prompt the users unless they bypass (we're installing stuff on their machine)
+  if (!force) {
+    ok <- yesno::yesno("This will install our app on your local library.
+                       Are you ok with that? ")
+  } else {
+    ok <- TRUE
+  }
+
+  # If user is ok, run npm install in the node folder in the package folder
+  # We should also check that the infra is not already there
+  if (ok){
+    processx::run(
+      command = "npm",
+      args = c("ci"),
+      wd = system.file("node", package = "rplaywright")
+    )
+
+    # processx::run(
+    #   command = "npx",
+    #   args = c("playwright", "install-deps"),
+    #   wd = system.file("node", package = "rplaywright")
+    # )
+
+    # processx::run(
+    #   command = "npx",
+    #   args = c("playwright", "install"),
+    #   wd = system.file("node", package = "rplaywright")
+    # )
+  }
+}
+
+#### start server ####
+#' @export
+rplaywright_start_server <- function(
+    force = FALSE
+){
+  playwright_server <<- processx::process$new(
+    command = "node",
+    args = c("src/server.js"),
+    wd = system.file("node", package = "rplaywright")
+  )
+}
+
+#' @export
+rplaywright_stop_server <- function(
+    force = FALSE
+){
+  if (exists("playwright_server")) {
+    playwright_server$kill_tree()
+  }
+}
+
+#' @export
+rplaywright_browser_new <- function(
+    force = FALSE, type = 'chromium'
+){
+  if (!exists("playwright_server")) {
+    rplaywright_start_server()
+    Sys.sleep(5)
+  }
+
+  if (!playwright_server$is_alive()) {
+    rplaywright_start_server()
+    Sys.sleep(5)
+  }
+
+  resp <- httr::POST('http://localhost:3000/browser/new', body = list(type = type), encode = "json", httr::accept_json())
+  httr::content(resp)
+}
+
+#' @export
+rplaywright_browser_close <- function(
+    browser
+){
+  resp <- httr::POST(paste0('http://localhost:3000/browser/close'), body = list(browser_id = browser$browser_id), encode = "json", httr::accept_json())
+  httr::content(resp)
+}
+
+#' @export
+rplaywright_context_new <- function(
+    browser, options = list()
+){
+  # jsonlite::toJSON(options, auto_unbox = T)
+  options$browser_id = browser$browser_id
+  resp <- httr::POST(paste0('http://localhost:3000/context/new'), body = options, encode = "json", httr::accept_json())
+  httr::content(resp)
+}
+
+#' @export
+rplaywright_context_close <- function(
+    context
+){
+  resp <- httr::POST(paste0('http://localhost:3000/context/close'), body = list(context_id = context$context_id), encode = "json", httr::accept_json())
+  httr::content(resp)
+}
+
+#' @export
+rplaywright_page_new <- function(
+    context, url, content, async = F
+){
+  body <- list(context_id = context$context_id, async = async)
+  if (!missing(url)) body$url <- url
+  if (!missing(content)) body$content <- content
+  resp <- httr::POST(paste0('http://localhost:3000/page/new'), body = body, encode = "json", httr::accept_json())
+  httr::content(resp)
+}
+
+#' @export
+rplaywright_page_close <- function(
+    page
+){
+  resp <- httr::POST(paste0('http://localhost:3000/page/close'), body = list(page_id = page$page_id), encode = "json", httr::accept_json())
+  httr::content(resp)
+}
+
+#' @export
+rplaywright_page_goto <- function(
+    page, url, async = F
+){
+  resp <- httr::POST(paste0('http://localhost:3000/page/goto'), body = list(page_id = page$page_id, url = url, async = async), encode = "json", httr::accept_json())
+  httr::content(resp)
+}
+
+#' @export
+rplaywright_page_set_content <- function(
+    page, content, async = F
+){
+  resp <- httr::POST(paste0('http://localhost:3000/page/set-content'), body = list(page_id = page$page_id, content = content, async = async), encode = "json", httr::accept_json())
+  httr::content(resp)
+}
+
+#' @export
+rplaywright_page_screenshot <- function(
+    page, path = NULL
+){
+  path = R.utils::getAbsolutePath(path)
+  resp <- httr::POST(paste0('http://localhost:3000/page/screenshot'), body = list(page_id = page$page_id, path = path), encode = "json", httr::accept_json())
+  httr::content(resp)
+}
+
+#' @export
+rplaywright_page_getbylabel <- function(
+    page, options = list()
+){
+  options$page_id = page$page_id
+  resp <- httr::POST(paste0('http://localhost:3000/page/getByLabel'), body = options, encode = "json", httr::accept_json())
+  httr::content(resp)
+}
+
+#' @export
+rplaywright_page_getbyrole <- function(
+    page, options = list()
+){
+  options$page_id = page$page_id
+  resp <- httr::POST(paste0('http://localhost:3000/page/getByRole'), body = options, encode = "json", httr::accept_json())
+  httr::content(resp)
+}
+
+#' @export
+rplaywright_page_waitforresponse <- function(
+    page, options = list()
+){
+  options$page_id = page$page_id
+  resp <- httr::POST(paste0('http://localhost:3000/page/waitForResponse'), body = options, encode = "json", httr::accept_json())
+  httr::content(resp)
+}
+
+#' @export
+rplaywright_page_scrollto <- function(
+    page, options = list()
+){
+  options$page_id = page$page_id
+  resp <- httr::POST(paste0('http://localhost:3000/page/scrollTo'), body = options, encode = "json", httr::accept_json())
+  httr::content(resp)
+}
+
+#' @export
+rplaywright_page_scrolltobottom <- function(
+    page, options = list()
+){
+  options$page_id = page$page_id
+  resp <- httr::POST(paste0('http://localhost:3000/page/scrollToBottom'), body = options, encode = "json", httr::accept_json())
+  httr::content(resp)
+}
+
+#' @export
+rplaywright_page_waitfortimeout <- function(
+    page, options = list()
+){
+  options$page_id = page$page_id
+  resp <- httr::POST(paste0('http://localhost:3000/page/waitForTimeout'), body = options, encode = "json", httr::accept_json())
+  httr::content(resp)
+}
---
+++ README.md
@@ -0,0 +1,105 @@
+# rplaywright
+
+rplaywright is an R package developed as part of my participation in the [rOpenSci Champions Program](https://ropensci.org/champions/). It is designed to seamlessly bridge with the NodeJS library, [Playwright](https://playwright.dev/), enabling simplified web testing and automation for R users. This package aims to provide R users with a comprehensive toolkit for interacting with web browsers programmatically, allowing for tasks such as browser automation, web scraping, and end-to-end testing directly from within the R environment. rplaywright offers a user-friendly interface and robust functionality for handling complex web interactions, making it an invaluable tool for both beginners and experienced developers alike.
+
+### Usage
+
+```R
+devtools::load_all()
+
+rplaywright::rplaywright_npm_install()
+
+browser <- rplaywright::rplaywright_browser_new(type = 'chromium') # or firefox or webkit
+context <- rplaywright::rplaywright_context_new(browser)
+
+page1 <- rplaywright::rplaywright_page_new(context, url = 'https://google.com')
+rplaywright::rplaywright_page_goto(page1, url = 'https://amazon.com', async=T)
+
+page2 <- rplaywright::rplaywright_page_new(context)
+rplaywright::rplaywright_page_set_content(page2, content = 'https://tokopedia.com', async=F)
+
+rplaywright::rplaywright_page_screenshot(page1, path = './ss1.png')
+rplaywright::rplaywright_page_screenshot(page2, path = './ss2.png')
+
+Sys.sleep(10)
+
+rplaywright::rplaywright_page_close(page1)
+rplaywright::rplaywright_page_close(page2)
+
+rplaywright::rplaywright_browser_close(browser)
+rplaywright::rplaywright_stop_server()
+```
+
+### Use Case Twitter Crawling
+
+```R
+roxygen2::roxygenise()
+
+browser <- rplaywright::rplaywright_browser_new(type = 'chromium') # or firefox or webkit
+context <- rplaywright::rplaywright_context_new(browser, options = list(
+    screen = list(width = 1240, height = 1080),
+    storageState = list(
+        cookies = list(
+            list(
+                name = "auth_token",
+                value = "ee488aa11b029d4e8e37550420c9aba3b5839088", # Use auth_token from cookies
+                domain = "twitter.com",
+                path = "/",
+                expires = -1,
+                httpOnly = T,
+                secure = T,
+                sameSite = "Strict"
+            )
+        ),
+        origins = list()
+    )
+))
+
+page1 <- rplaywright::rplaywright_page_new(context, url = 'https://twitter.com/search-advanced')
+
+rplaywright::rplaywright_page_getbylabel(page1, options = list(
+    text = "All of these words",
+    actions = list(
+        list(fill = list(""))
+    )
+))
+
+rplaywright::rplaywright_page_getbyrole(page1, options = list(
+    role = "button",
+    options = list(name = "Search"),
+    actions = list(
+        list(click = list())
+    )
+))
+
+result <- list()
+count <- 1
+while (T) {
+    resp <- rplaywright::rplaywright_page_waitforresponse(page1, options = list(
+        timeout = 5000,
+        filter = list(or = list(
+            list(url = "SearchTimeline"), 
+            list(url = "TweetDetail")
+        ))
+    ))
+
+    if (is.null(resp$statusCode)) resp$statusCode = 200L
+
+    if (!httr::http_error(resp$statusCode) && resp$success == T) {
+        result[[count]] <- resp
+        count <- count + 1
+    }
+
+    rplaywright::rplaywright_page_scrollto(page1, options = list(to = 0))
+
+    rplaywright::rplaywright_page_waitfortimeout(page1, options = list(timeout = 2000))
+
+    rplaywright::rplaywright_page_scrolltobottom(page1, options = list())
+
+    if (count > 10) break;
+}
+
+rplaywright::rplaywright_page_close(page1)
+rplaywright::rplaywright_browser_close(browser)
+rplaywright::rplaywright_stop_server()
+```
